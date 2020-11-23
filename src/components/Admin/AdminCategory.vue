0ae1d802f98ab8bd8a029c797b17cfbc6a0990f6
dstark <gorokhov.dv93@gmail.com>
2020-11-23 19:05:05 +0300

    add get and delite methods in AdminCategory

--- src/components/Admin/AdminCategory.vue
+++ src/components/Admin/AdminCategory.vue
@@ -13,13 +13,15 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>id</td>
-          <td>name</td>
-          <td>img</td>
-          <td>info</td>
+        <tr v-for="(cat, index) in categories" v-bind:key="cat.id">
+          <td>{{ cat.id }}</td>
+          <td>{{ cat.name }}</td>
+          <td>{{ cat.url }}</td>
+          <td>{{ cat.info }}</td>
           <td><button class="btn btn-warning">1</button></td>
-          <td><button class="btn btn-danger">2</button></td>
+          <td>
+            <button class="btn btn-danger" @click="delete_cat(index)">2</button>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -27,7 +29,55 @@
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  mounted() {
+    this.getCategories();
+  },
+  data() {
+    return {
+      categories: [],
+    };
+  },
+  methods: {
+    getCategories() {
+      axios
+        .get("http://127.0.0.1:8000/api/auth/category", {
+          headers: {
+            Authorization:
+              "Bearer " + JSON.parse(localStorage.getItem("token")),
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+          this.categories = response.data;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    delete_cat(index) {
+      axios
+        .delete(
+          "http://127.0.0.1:8000/api/auth/category" +
+            "/" +
+            this.categories[index].id,
+          {
+            headers: {
+              Authorization:
+                "Bearer " + JSON.parse(localStorage.getItem("token")),
+            },
+          }
+        )
+        .then(() => {
+          this.getCategories();
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
